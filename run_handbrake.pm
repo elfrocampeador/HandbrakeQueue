2816d4c3cd13d1654c832e67ae355d5672da3f25
Matthew Treinish <mtreinish@kortar.org>
2016-11-10 12:51:52 -0500

    Use bash to handle stderr redirection
    I can't seem to find a good model for doing stderr redirection in perl.
    Maybe one day I'll figure out how to do this natively in perl, but for
    now lets just use something I know works. This commit adds a bash
    wrapper to calling HandBrakeCLI which will do the redirection and then
    the run() function is updated to call that script instead.

--- run_handbrake.pm
+++ run_handbrake.pm
@@ -57,7 +57,8 @@ sub run {
     if( find_handbrake_cli() == 0) {
         die "ERROR: Can't find HandBrakeCLI executable in the PATH";
     }
-    my @cmd_args = ('HandBrakeCLI', '-i', $infile, '-o', $outfile);
+    my @cmd_args = ('./run_handbrake.sh', $log_path, '-i', $infile, '-o',
+                    $outfile);
     if( defined $title) {
         push @cmd_args, '-t', $title;
     }
@@ -90,10 +91,8 @@ sub run {
     if( defined $encoder_preset) {
         push @cmd_args, '--encoder-preset', $encoder_preset;
     }
-    # Capture stdout and stderr
-    push @cmd_args, '&>', $log_path;
     # Run the command
-    return system(join(' ', @cmd_args));
+    return system(@cmd_args);
 }
 
 1;
